8c01edd43c8a057c60223ebb16b33931db2a18ac
nayan0210 <nayanpandey2001@gmail.com>
2023-12-14 14:02:43 +0530

    ghfhfgh

---
+++ src/com/jsp/flipkart/User.java
@@ -0,0 +1,33 @@
+package com.jsp.flipkart;
+
+public class User {
+	private int userId;
+	private String userName;
+	private String userEmail;
+	private String userPaasword;
+	public String getUserEmail() {
+		return userEmail;
+	}
+	public void setUserEmail(String userEmail) {
+		this.userEmail = userEmail;
+	}
+	public String getUserPaasword() {
+		return userPaasword;
+	}
+	public void setUserPaasword(String userPaasword) {
+		this.userPaasword = userPaasword;
+	}
+	public int getUserId() {
+		return userId;
+	}
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+}
